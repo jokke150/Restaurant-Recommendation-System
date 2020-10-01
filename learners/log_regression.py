1111ff3592dbbc974b6508ae0aae19291c753462
Kelian1 <49244794+Kelian1@users.noreply.github.com>
2020-10-01 18:08:57 +0200

    Update log_regression.py

--- learners/log_regression.py
+++ learners/log_regression.py
@@ -1,15 +1,16 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-
 def logistic_regression(x_train, x_test, y_test, y_train):
+    
+   
     vectorizer = CountVectorizer()
     vectorizer.fit(x_train)
 
     x_train = vectorizer.transform(x_train)
     x_test = vectorizer.transform(x_test)
-
-    classifier = LogisticRegression()
-    classifier.fit(x_train, y_train)
+    
+    classifier = LogisticRegression(class_weight="balanced").fit(x_train, y_train)
+  
 
     score = classifier.score(x_test, y_test)
     print("Accuracy:", score)
