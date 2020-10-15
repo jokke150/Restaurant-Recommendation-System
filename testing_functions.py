71a3761062af7c2cc69448bee65e12bbbce81e2e
Heeycoen <Coen.kenter@me.com>
2020-10-15 20:22:18 +0200

    new data

--- testing_functions.py
+++ testing_functions.py
@@ -9,26 +9,25 @@ lensplit = 4
 shortwordmax = 1
 longwordmax = 3
 
-file = open("data.csv","w")
+file = open("data2.csv","w")
 
 def create_test_data():
     file1 = open('data/all_dialogs.txt', 'r')
     Lines = file1.readlines()
     datalines = []
-    file.write("line,lev0food,lev1food,typ0food,typ1food,"
-               "lev0area,lev1area,typ0area,typ1area,"
-               "lev0price,lev1price,typ0price,typ1price,"
-               "lev0add,lev1add,typ0add,typ1add\n")
+    file.write("line,lev0,lev1,typ0,typ1\n")
     for i in range(0,len(Lines)):
         if Lines[i] == "turn index: 0\n":
             datalines.append(i+2)
-
     data = list(map(lambda x: Lines[x][len("user: "):][:-len("\n")], datalines))
+    random.shuffle(data)
     data = list(filter(lambda x: x != "noise" and x != "static" and x != "sil" and x != "unintelligible" and
                                  x != "cough", data))
+    ##data = list(map(lambda x: generateTypos(x,3),data))
     print(len(data))
     data = list(map(lambda x: get_all_finds(x),data))
     file.close()
+
 
 
 def get_all_finds(sentence):
@@ -42,36 +41,42 @@ def get_all_finds(sentence):
             typo_vs_levenshtein(words, price_ranges),
             typo_vs_levenshtein(words, ADD_REQ_KEYWORDS)]
 
-    if lst[0] is not None or lst[1] is not None or lst[2] is not None or lst[3] is not None:
+    x = [[lst[0][0], lst[1][0], lst[2][0], lst[3][0]], [lst[0][1], lst[1][1], lst[2][1], lst[3][1]],
+         [lst[0][2], lst[1][2], lst[2][2], lst[3][2]], [lst[0][3], lst[1][3], lst[2][3], lst[3][3]]]
 
-        file.write(sentence + tostring(lst) + "\n")
+    if not (x[0] == x[1] and x[1] == x[2] and lst[2] == x[3]):
+
+        file.write(sentence + tostring(x) + "\n")
         return lst
     return
 
 def tostring(lst):
-    string = ""
+    string=  ""
     for i in lst:
-        if i is not None:
-            for j in i:
-                string += ","
-                if len(j) != 0:
-                    string += str(j[1])
-
-        else:
-            string += ",,,,"
-
-
+        string += "," + tostrings(i)
 
     return string
+
+def tostrings(s):
+
+    string = "Do you want a restaurant "
+    if len(s[0]) != 0:
+        string += "with "+ s[0][1] + " cuisine "
+    if len(s[1]) != 0:
+        string += "in the " + s[1][1] + " part of town "
+
+    if len(s[2]) != 0:
+        string += "at a " + s[2][1] + " price range "
+
+    if len(s[3]) != 0:
+        string += "with this additional requirement: " + s[3][1]
+    string += "."
+    return string
+
 
 def typo_vs_levenshtein(lst, words):
     lev1 ,lev2= matched_words_in_split(lst, words, "")
     typ1, typ2= matched_words_in_split(lst, words, "typoDistance")
-    if len(lev1) == 0 and len(lev2) == 0 and len(typ1) == 0 and len(typ2) == 0:
-        return
-    if len(lev1) > 0 and len(lev2) > 0 and len(typ1) > 0 and len(typ2) > 0:
-        if lev1[1] == lev2[1] and lev2[1] == typ1[1] and typ1[1] == typ2[1]:
-            return
     return (lev1,lev2,typ1,typ2)
 
 def matched_words_in_split(lst, words, func,diff=0):
@@ -109,7 +114,8 @@ def filt(word,closest_distance, closest_words,diff):
 def generateTypos(sentence, distance):
     generator = typoGenerator(sentence, distance)
     for d in range(0, 20):
-        print(generator.__next__())
+        generator.__next__()
+    return generator.__next__()
 
 
 if __name__ == '__main__':
